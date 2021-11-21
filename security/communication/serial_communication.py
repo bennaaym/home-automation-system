73f650ab64bd0663ea8aeb8b8273fa89283c1482
bennaaym <mr.aymen.bennabi@gmail.com>
2021-11-22 01:05:44 +0300

    bennaaym/security: Serial class implemented

---
+++ security/communication/serial_communication.py
@@ -0,0 +1,36 @@
+from typing import Any
+import serial 
+import time
+
+
+class Serial:
+    """
+    Handle serial communication between the python script and the microcontroller
+    """
+
+    # CONSTRUCTOR
+    def __init__(self,PORT:str = 'COM3',BAUD_RATE:int = 9600,TIMEOUT:float = .1) -> None:
+        self.__port:str = PORT
+        self.__baud_rate:int = BAUD_RATE
+        self.__timeout:float = TIMEOUT
+        self.__serial:Any = None
+        
+    # PUBLIC METHODS
+    def read(self) -> str:
+        self.__connect()
+        return self.__serial.read()
+
+    def write(self,message)-> None:
+        self.__connect()
+        self.__serial.write(bytes(message,'utf-8'))
+    
+
+    def close(self)-> None:
+        self.__serial.close()
+        self.__serial = None
+
+    # PRIVATE METHODS
+    def __connect(self)-> None :
+        if not self.__serial : 
+            self.__serial = serial.Serial(self.__port, self.__baud_rate, timeout = self.__timeout)
+            
