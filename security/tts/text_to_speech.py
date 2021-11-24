1bfc80ced656975c81716cc2dbf1b2abcfe837fe
bennaaym <mr.aymen.bennabi@gmail.com>
2021-11-24 17:23:15 +0300

    bennaaym/security: TTS class updated to use Singleton design pattern, new messages added to Messages enumerator

--- security/tts/text_to_speech.py
+++ security/tts/text_to_speech.py
@@ -3,22 +3,40 @@ import pyttsx3
 class TTS:
     """
     Changes text to speech
+    This class uses Singleton design pattern
     """
 
-    def __init__(
-        self,
+    # SINGLETON INSTANCE
+    __instance = None
+
+    @staticmethod
+    def get_instance(
         rate:int = 130,
         lang_id:str = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0',
-    )-> None:
+    ):
+        if TTS.__instance == None:
+            TTS.__instance = TTS(rate,lang_id)
 
-        self.engine = pyttsx3.init()
-        self.engine.setProperty('voice',lang_id)
-        self.engine.setProperty('rate',rate)
+        return TTS.__instance 
 
 
+    # PRIVATE CONSTRUCTOR
+    def __init__(self,rate:int = None,lang_id:str = None) -> None:
+        
+        # checks if there is an existing instance of TTS
+        if TTS.__instance:
+            raise Exception("Singleton cannot be instantiated more than once")
+        
+        else :
+            self.engine = pyttsx3.init()
+            self.engine.setProperty('voice',lang_id)
+            self.engine.setProperty('rate',rate)
+            TTS.__instance = self
+
+
+    # PUBLIC METHODS
     def speak(self,message)-> None:
-        if self.engine._inLoop:
-            self.engine.endLoop()
+        if self.engine._inLoop:  self.engine.endLoop()
         self.engine.say(message)
         self.engine.runAndWait()
         self.engine.stop()
