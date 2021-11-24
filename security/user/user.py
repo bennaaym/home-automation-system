29b7731f7bb69554e576f414335ab95a87cf39e0
bennaaym <mr.aymen.bennabi@gmail.com>
2021-11-24 17:25:59 +0300

    bennaaym/security: User class implemented

---
+++ security/user/user.py
@@ -0,0 +1,22 @@
+
+class User:
+    """
+    Stores the information of the current user 
+    """
+
+    def __init__(self, name:str, picture_url:str) -> None:
+        self.__name = name
+        self.__picture_url = picture_url
+
+
+    @property
+    def name(self)-> str:
+        return self.__name
+
+    @property
+    def picture(self)-> str:
+        return self.__picture_url
+
+
+    def __str__(self) -> str:
+        return f"name: {self.__name}, picture: {self.__picture_url}"
