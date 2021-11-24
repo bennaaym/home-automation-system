52c264947021737c2fe2c0ed73e5af23edef88c6
bennaaym <mr.aymen.bennabi@gmail.com>
2021-11-24 20:10:09 +0300

    bennaaym/security: QRRecognizer class updated to use Singleton design pattern

--- security/recognition/qr_recognizer.py
+++ security/recognition/qr_recognizer.py
@@ -2,62 +2,90 @@ from threading import Thread
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
+from typing import List,Any, Union
+
+from database.firestore import Firestore
+from user.user import User
 from tts.messages import Messages
 from tts.text_to_speech import TTS
-from db.db import DB
-from typing import List,Any
 from .util import terminate_thread
 
 
 class QRRecognizer:
     """
     Detect and validate a the QR code showing to the camera by the user 
+    This class uses Singleton design pattern
     """
 
-    # CONSTRUCTOR 
-    def __init__(self,capture,db:DB)-> None:
-        self.capture = capture
-        self.tts = TTS()
-        self.tts_thread = None
-        self.threads:List[Thread] = []
-        self.thread_killers:List[Thread] = []
-        self.is_running = True
-        self.db = db
+    # SINGLETON INSTANCE
+    __instance = None
+
+    @staticmethod
+    def get_instance(capture):
+        if QRRecognizer.__instance == None:
+            QRRecognizer.__instance = QRRecognizer(capture)
+
+        return QRRecognizer.__instance
+
+    
+
+    # PRIVATE CONSTRUCTOR 
+    def __init__(self,capture)-> None:
+
+        # checks if there is an existing instance of Firestore
+        if QRRecognizer.__instance:
+            raise Exception("Singleton cannot be instantiated more than once")
+
+        else: 
+            self.capture = capture
+            self.__tts = TTS.get_instance()
+            self.__tts_thread = None
+            self.__threads:List[Thread] = []
+            self.__thread_killers:List[Thread] = []
+            self.__is_running = True
+            self.__user:Union[User, None] = None
+            self.__db = Firestore.get_instance()
+            QRRecognizer.__instance = self
 
     # PUBLIC METHODS
-    def run(self)-> bool:
+    def run(self)-> Union[User,None]:
                 
-        while self.is_running:
+        while self.__is_running:
+
+            # gets current frame from the video stream
             _, frame = self.capture.read()
+            
+            # decodes qr code if any
             qr_data = self.__get_qr_data(frame)
 
 
             if len(qr_data) == 0:
                 cv2.imshow("QR RECOGNITION", frame)
 
-                
-                killer = Thread(target=terminate_thread,args=(self.threads,))
-                self.thread_killers.append(killer)
+                # creates a killer thread to kill tts running __threads
+                killer = Thread(target=terminate_thread,args=(self.__threads,))
+                self.__thread_killers.append(killer)
                 killer.start()            
 
             else:
+                # decodes QR code
+                # frames the detected QR code
                 for obj in qr_data:
                     key = obj.data.decode('utf-8')
                     frame  = self.__draw_polygon(frame, obj)
                     cv2.imshow("QR RECOGNITION", frame)
                     
-                # creates a different thread for db interaction
-                # check if the QR key exists in the database
-                if len(self.threads) == 0:
-                    self.tts_thread = Thread(target = self.__check_qr_key, args=(key,))
-                    self.threads.append(self.tts_thread)
-                    self.tts_thread.start()
+                # creates a different thread for database interaction and voice messages
+                if len(self.__threads) == 0:
+                    self.__tts_thread = Thread(target = self.__check_qr_key, args=(key,))
+                    self.__threads.append(self.__tts_thread)
+                    self.__tts_thread.start()
                     
             if cv2.waitKey(1) & 0xFF == ord('q'):
                break
        
         cv2.destroyAllWindows()
-        return not self.is_running
+        return self.__user
 
     # PRIVATE METHODS
     def __get_qr_data(self,input_frame)-> List[Any]:
@@ -74,9 +102,14 @@ class QRRecognizer:
         return frame
 
 
-    def __check_qr_key(self,key)-> bool:
-        if self.db.check_key(key):
-            self.tts.speak(Messages.VALID_QR_CODE)
-            self.is_running = False
+    def __check_qr_key(self,key)-> None:
+
+        user = self.__db.connect(key)
+
+        if user:
+            self.__tts.speak(Messages.VALID_QR_CODE)
+            self.__is_running = False
+            self.__user = user
+
         else:
-            self.tts.speak(Messages.INVALID_QR_CODE)
+           self.__tts.speak(Messages.INVALID_QR_CODE)
--- security/recognition/util.py
+++ security/recognition/util.py
@@ -2,6 +2,8 @@ from threading import Thread
 from typing import List
 
 def terminate_thread(threads:List[Thread]):
-    for thread in  threads:
-        thread.join()
+    """
+    Utility function used to kill threads
+    """
+    for thread in  threads: thread.join()
     threads.clear()   
