af690d66a6e709dfe18d51f98e91c2cda85d1395
hajer gafsi <hajergafsi15@yahoo.fr>
2021-11-27 15:14:26 +0300

    removed some unnecessary lines

--- control/react-native-app/App.js
+++ control/react-native-app/App.js
@@ -14,15 +14,6 @@ const channelNo = "1580471";
 const apiWriteKey = "E1IEL3E8446RN0I8";
 
 export default function App() {
-  const dispatch = useDispatch();
-  
-  const [tempertaure, setTemperature] = useState(null);
-
-
-  useEffect(() => {
-    axios.get(`https://api.thingspeak.com/channels/1580471/fields/3/last.json`).then((response) => {  
-    setTemperature(response.data.field3);
-  })})
 
   return (
     <View style={styles.container}>
@@ -34,12 +25,6 @@ export default function App() {
     </View>
   );
 }
-
-const headers = {
-  'Content-Type': 'application/json',
-}
-
-
 
 const styles = StyleSheet.create({
   container: {
