f8e30d5af1be07e28284bbfd369ec780c59e83f7
hajer gafsi <hajergafsi15@yahoo.fr>
2021-12-23 15:29:33 +0300

    + Arduino codes

---
+++ control/micro-controllers/led_thingspeak.ino
@@ -0,0 +1,62 @@
+#include<ThingSpeak.h>
+#include<ESP8266WiFi.h>
+#include<ESP8266WebServer.h>
+unsigned long channel_num=1580471;
+const char ssid[]="OPPO Reno3";
+const char password[]="";
+
+const char* writeAPIKey = "E1IEL3E8446RN0I8";     //  Thingspeak  write API Key
+const char* readAPIKey  = "SRMRPASNKDIMQPBX";
+
+int kitchen_lamp;
+int living_room_lamp;
+
+unsigned int value;
+
+WiFiClient client;   // make the client of the WiFi which connect to the ThingSpeak webServer
+ESP8266WebServer server(80); 
+void setup() {
+   Serial.begin(115200);
+  // put your setup code here, to run once:
+   pinMode (A0,  INPUT); 
+   pinMode(D1,OUTPUT);
+   pinMode(D3,OUTPUT);
+   digitalWrite(D1,0); 
+   digitalWrite(D3,0); 
+   WiFi.begin(ssid,password);    // connect to the wifi STA connection
+   while(WiFi.status()!=WL_CONNECTED)
+   {
+     delay(500);
+     Serial.print(".");
+   }
+   Serial.println(WiFi.localIP());    // print the wifi local ip
+   ThingSpeak.begin(client);      // connect the client to the thingSpeak server
+   server.begin(); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+     server.handleClient();    // it realy handle the Client
+     kitchen_lamp=ThingSpeak.readFloatField(channel_num,1);    // rad the last data of the field 1
+     living_room_lamp=ThingSpeak.readFloatField(channel_num,2);    // rad the last data of the field 1
+     //blue led control 
+     if(kitchen_lamp==0)
+     {
+       digitalWrite(D1,1);
+     }
+     else if(kitchen_lamp==1)
+     {
+       digitalWrite(D1,0);
+     }
+     
+     //red led control 
+     if(living_room_lamp==0)
+     {
+       digitalWrite(D3,1);
+     }
+     else if(living_room_lamp==1)
+     {
+       digitalWrite(D3,0);
+     }
+
+}
---
+++ control/micro-controllers/servo.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+#include<ThingSpeak.h>
+#include<ESP8266WiFi.h>
+#include<ESP8266WebServer.h>
+
+unsigned long channel_num=1580471;
+const char ssid[]="OPPO Reno3";
+const char password[]="";
+
+const char* writeAPIKey = "E1IEL3E8446RN0I8";     //  Thingspeak  write API Key
+const char* readAPIKey  = "SRMRPASNKDIMQPBX";
+
+WiFiClient client;   // make the client of the WiFi which connect to the ThingSpeak webServer
+ESP8266WebServer server(80); 
+
+Servo servo;
+
+void setup() {
+   Serial.begin(115200);
+   WiFi.begin(ssid,password);    // connect to the wifi STA connection
+   while(WiFi.status()!=WL_CONNECTED)
+   {
+     delay(500);
+     Serial.print(".");
+   }
+   servo.attach(2); //D4
+   servo.write(0);
+   delay(2000);
+   Serial.println(WiFi.localIP());    // print the wifi local ip
+  // in urt type the "/" then call the handle on connect function
+   ThingSpeak.begin(client);      // connect the client to the thingSpeak server
+   server.begin(); 
+}
+
+void loop() {
+  server.handleClient();  
+  servo.write(ThingSpeak.readFloatField(channel_num,4));
+}
